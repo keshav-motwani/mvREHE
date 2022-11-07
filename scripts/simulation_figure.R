30a1fd424d93f0a1ea0d9880f80670c197cdd81f
Keshav Motwani <keshavmot2@gmail.com>
2022-11-07 06:48:13 -0800

    update figure script

--- scripts/simulation_figure.R
+++ scripts/simulation_figure.R
@@ -9,13 +9,15 @@ files = files[grepl("rds", files)]
 
 results = lapply(files, readRDS)
 
-error_df = do.call(rbind, lapply(results, function(x) x$spectral_error))
-error_df$error = error_df$spectral_error
+spectral_error_df = do.call(rbind, lapply(results, function(x) x$spectral_error))
+spectral_error_df$error = spectral_error_df$spectral_error
+EV1_error_df = do.call(rbind, lapply(results, function(x) x$EV1_error))
+EV1_error_df$error = EV1_error_df$EV1_error
 time_df = do.call(rbind, lapply(results, function(x) x$time))
 truncated = do.call(rbind, lapply(results, function(x) x$truncated))
 min_eigenvalue_df = do.call(rbind, lapply(results, function(x) x$min_eigenvalue))
 
-truncated_proportion = truncated %>% 
+truncated_proportion = truncated %>%
   group_by(replicate, n, q, experiment) %>%
   summarize(truncated = max(truncated)) %>%
   group_by(n, q, experiment) %>%
@@ -24,7 +26,9 @@ truncated_proportion = truncated %>%
 truncated_proportion1 = truncated_proportion
 truncated_proportion = do.call(rbind, lapply(unique(truncated$estimate), function(x) truncated_proportion %>% mutate(estimate = x)))
 
-error_df = left_join(error_df, truncated %>% mutate(method = NULL) %>% group_by(n, q, replicate, experiment) %>% summarize(truncated = max(truncated)), by = c("n", "q", "replicate", "experiment")) %>%
+spectral_error_df = left_join(spectral_error_df, truncated %>% mutate(method = NULL) %>% group_by(n, q, replicate, experiment) %>% summarize(truncated = max(truncated)), by = c("n", "q", "replicate", "experiment")) %>%
+  mutate(truncated = ifelse(truncated == 0, "not truncated", "truncated"))
+EV1_error_df = left_join(EV1_error_df, truncated %>% mutate(method = NULL) %>% group_by(n, q, replicate, experiment) %>% summarize(truncated = max(truncated)), by = c("n", "q", "replicate", "experiment")) %>%
   mutate(truncated = ifelse(truncated == 0, "not truncated", "truncated"))
 
 time_df = left_join(time_df, truncated %>% mutate(method = NULL) %>% group_by(n, q, replicate, experiment) %>% summarize(truncated = max(truncated)), by = c("n", "q", "replicate", "experiment")) %>%
@@ -32,29 +36,57 @@ time_df = left_join(time_df, truncated %>% mutate(method = NULL) %>% group_by(n,
 
 palette = c("mvHE" = "#006E7F", "mvREHE" = "#EE5007", "mvREHE_init" = "#EE5007", "mvREHE_optim" = "#EE5007", "mvREML" = "#4DBBD5B2", "naive" = "gray")
 
-ggplot(error_df %>% filter(experiment == "n") %>% mutate(facet = paste0("q = ", q, "; ", estimate)) %>% mutate(facet = factor(facet, levels = gtools::mixedsort(unique(facet)))), aes(x = as.factor(n), y = error, color = method)) +
+ggplot(spectral_error_df %>% filter(experiment == "n") %>% mutate(facet = paste0("q = ", q, "; ", estimate)) %>% mutate(facet = factor(facet, levels = gtools::mixedsort(unique(facet)))), aes(x = as.factor(n), y = error, color = method)) +
   facet_wrap(~facet, scales = "free_y", ncol = 2) +
   geom_boxplot(outlier.size = 0.25) +
   geom_text(data = truncated_proportion %>% filter(experiment == "n") %>% mutate(facet = paste0("q = ", q, "; ", estimate)) %>% mutate(facet = factor(facet, levels = gtools::mixedsort(unique(facet)))), aes(label = paste0(round(proportion * 100, 2), "%"), y = Inf, vjust = 2), size = 2.5) +
   theme_bw() +
   xlab("n") +
-  ylab("Estimation error") +
+  ylab("Spectral error") +
   labs(color = "Method") +
   scale_color_manual(values = palette) +
   theme(legend.position = "bottom")
-ggsave(file.path(FIGURES_PATH, "simulation_figure_error_n.pdf"), height = 4, width = 7)
+ggsave(file.path(FIGURES_PATH, "simulation_figure_spectral_error_n.pdf"), height = 4, width = 7)
 
-ggplot(error_df %>% filter(experiment == "n") %>% mutate(facet = paste0("q = ", q, "; ", estimate)) %>% mutate(facet = factor(facet, levels = gtools::mixedsort(unique(facet)))), aes(x = as.factor(n), y = error, color = method)) +
+ggplot(spectral_error_df %>% filter(experiment == "n") %>% mutate(facet = paste0("q = ", q, "; ", estimate)) %>% mutate(facet = factor(facet, levels = gtools::mixedsort(unique(facet)))), aes(x = as.factor(n), y = error, color = method)) +
   facet_grid(facet ~ truncated, scales = "free_y") +
   geom_boxplot(outlier.size = 0.25) +
-  geom_text(data = error_df %>% filter(method == "mvHE", experiment == "n") %>% group_by(n, estimate, q, truncated, method) %>% summarize(count = n()) %>% group_by(n, estimate, q, method) %>% mutate(proportion = count / sum(count)) %>% mutate(facet = paste0("q = ", q, "; ", estimate)) %>% mutate(facet = factor(facet, levels = gtools::mixedsort(unique(facet)))), aes(label = paste0(round(proportion * 100, 2), "%"), y = Inf, vjust = 2), size = 2.5) +
+  geom_text(data = spectral_error_df %>% filter(method == "mvHE", experiment == "n") %>% group_by(n, estimate, q, truncated, method) %>% summarize(count = n()) %>% group_by(n, estimate, q, method) %>% mutate(proportion = count / sum(count)) %>% mutate(facet = paste0("q = ", q, "; ", estimate)) %>% mutate(facet = factor(facet, levels = gtools::mixedsort(unique(facet)))), aes(label = paste0(round(proportion * 100, 2), "%"), y = Inf, vjust = 2), size = 2.5) +
   theme_bw() +
   xlab("n") +
-  ylab("Estimation error") +
+  ylab("Spectral error") +
   labs(color = "Method") +
   scale_color_manual(values = palette) +
   theme(legend.position = "bottom")
-ggsave(file.path(FIGURES_PATH, "simulation_figure_error_truncated_n.pdf"), height = 8, width = 7)
+ggsave(file.path(FIGURES_PATH, "simulation_figure_spectral_error_truncated_n.pdf"), height = 8, width = 7)
+
+
+
+ggplot(EV1_error_df %>% filter(experiment == "n") %>% mutate(facet = paste0("q = ", q, "; ", estimate)) %>% mutate(facet = factor(facet, levels = gtools::mixedsort(unique(facet)))), aes(x = as.factor(n), y = error, color = method)) +
+  facet_wrap(~facet, scales = "free_y", ncol = 2) +
+  geom_boxplot(outlier.size = 0.25) +
+  geom_text(data = truncated_proportion %>% filter(experiment == "n") %>% mutate(facet = paste0("q = ", q, "; ", estimate)) %>% mutate(facet = factor(facet, levels = gtools::mixedsort(unique(facet)))), aes(label = paste0(round(proportion * 100, 2), "%"), y = Inf, vjust = 2), size = 2.5) +
+  theme_bw() +
+  xlab("n") +
+  ylab("Eigenvector 1 error") +
+  labs(color = "Method") +
+  scale_color_manual(values = palette) +
+  theme(legend.position = "bottom")
+ggsave(file.path(FIGURES_PATH, "simulation_figure_EV1_error_n.pdf"), height = 4, width = 7)
+
+ggplot(EV1_error_df %>% filter(experiment == "n") %>% mutate(facet = paste0("q = ", q, "; ", estimate)) %>% mutate(facet = factor(facet, levels = gtools::mixedsort(unique(facet)))), aes(x = as.factor(n), y = error, color = method)) +
+  facet_grid(facet ~ truncated, scales = "free_y") +
+  geom_boxplot(outlier.size = 0.25) +
+  geom_text(data = EV1_error_df %>% filter(method == "mvHE", experiment == "n") %>% group_by(n, estimate, q, truncated, method) %>% summarize(count = n()) %>% group_by(n, estimate, q, method) %>% mutate(proportion = count / sum(count)) %>% mutate(facet = paste0("q = ", q, "; ", estimate)) %>% mutate(facet = factor(facet, levels = gtools::mixedsort(unique(facet)))), aes(label = paste0(round(proportion * 100, 2), "%"), y = Inf, vjust = 2), size = 2.5) +
+  theme_bw() +
+  xlab("n") +
+  ylab("Eigenvector 1 error") +
+  labs(color = "Method") +
+  scale_color_manual(values = palette) +
+  theme(legend.position = "bottom")
+ggsave(file.path(FIGURES_PATH, "simulation_figure_EV1_error_truncated_n.pdf"), height = 8, width = 7)
+
+
 
 ggplot(min_eigenvalue_df %>% filter(experiment == "n") %>% mutate(facet = paste0("q = ", q, "; ", estimate)) %>% mutate(facet = factor(facet, levels = gtools::mixedsort(unique(facet)))), aes(x = as.factor(n), y = min_eigenvalue, color = method)) +
   facet_wrap(~facet, scales = "free_y", ncol = 2) +
@@ -91,29 +123,57 @@ ggplot(time_df %>% filter(experiment == "n") %>% mutate(facet = paste0("q = ", q
 ggsave(file.path(FIGURES_PATH, "simulation_figure_time_truncated_n.pdf"), height = 4, width = 7)
 
 
-ggplot(error_df %>% filter(experiment == "q"), aes(x = as.factor(q), y = error, color = method)) +
+ggplot(spectral_error_df %>% filter(experiment == "q"), aes(x = as.factor(q), y = error, color = method)) +
   facet_wrap(~paste0("n = ", n, "; ", estimate), scales = "free_y", ncol = 2) +
   geom_boxplot(outlier.size = 0.25) +
   geom_text(data = truncated_proportion %>% filter(experiment == "q"), aes(label = paste0(round(proportion * 100, 2), "%"), y = Inf, vjust = 2), size = 2.5) +
   theme_bw() +
   xlab("q") +
-  ylab("Estimation error") +
+  ylab("Spectral error") +
   labs(color = "Method") +
   scale_color_manual(values = palette) +
   theme(legend.position = "bottom")
-ggsave(file.path(FIGURES_PATH, "simulation_figure_error_q.pdf"), height = 4, width = 7)
+ggsave(file.path(FIGURES_PATH, "simulation_figure_spectral_error_q.pdf"), height = 4, width = 7)
 
-ggplot(error_df %>% filter(experiment == "q"), aes(x = as.factor(q), y = error, color = method)) +
+ggplot(spectral_error_df %>% filter(experiment == "q"), aes(x = as.factor(q), y = error, color = method)) +
   facet_grid(paste0("n = ", n, "; ", estimate) ~ truncated, scales = "free_y") +
   geom_boxplot(outlier.size = 0.25) +
-  geom_text(data = error_df %>% filter(method == "mvHE", experiment == "q") %>% group_by(n, estimate, q, truncated, method) %>% summarize(count = n()) %>% group_by(n, estimate, q, method) %>% mutate(proportion = count / sum(count)), aes(label = paste0(round(proportion * 100, 2), "%"), y = Inf, vjust = 2), size = 2.5) +
+  geom_text(data = spectral_error_df %>% filter(method == "mvHE", experiment == "q") %>% group_by(n, estimate, q, truncated, method) %>% summarize(count = n()) %>% group_by(n, estimate, q, method) %>% mutate(proportion = count / sum(count)), aes(label = paste0(round(proportion * 100, 2), "%"), y = Inf, vjust = 2), size = 2.5) +
   theme_bw() +
   xlab("q") +
-  ylab("Estimation error") +
+  ylab("Spectral error") +
   labs(color = "Method") +
   scale_color_manual(values = palette) +
   theme(legend.position = "bottom")
-ggsave(file.path(FIGURES_PATH, "simulation_figure_error_truncated_q.pdf"), height = 8, width = 7)
+ggsave(file.path(FIGURES_PATH, "simulation_figure_spectral_error_truncated_q.pdf"), height = 8, width = 7)
+
+
+
+ggplot(EV1_error_df %>% filter(experiment == "q"), aes(x = as.factor(q), y = error, color = method)) +
+  facet_wrap(~paste0("n = ", n, "; ", estimate), scales = "free_y", ncol = 2) +
+  geom_boxplot(outlier.size = 0.25) +
+  geom_text(data = truncated_proportion %>% filter(experiment == "q"), aes(label = paste0(round(proportion * 100, 2), "%"), y = Inf, vjust = 2), size = 2.5) +
+  theme_bw() +
+  xlab("q") +
+  ylab("Eigenvector 1 error") +
+  labs(color = "Method") +
+  scale_color_manual(values = palette) +
+  theme(legend.position = "bottom")
+ggsave(file.path(FIGURES_PATH, "simulation_figure_EV1_error_q.pdf"), height = 4, width = 7)
+
+ggplot(EV1_error_df %>% filter(experiment == "q"), aes(x = as.factor(q), y = error, color = method)) +
+  facet_grid(paste0("n = ", n, "; ", estimate) ~ truncated, scales = "free_y") +
+  geom_boxplot(outlier.size = 0.25) +
+  geom_text(data = EV1_error_df %>% filter(method == "mvHE", experiment == "q") %>% group_by(n, estimate, q, truncated, method) %>% summarize(count = n()) %>% group_by(n, estimate, q, method) %>% mutate(proportion = count / sum(count)), aes(label = paste0(round(proportion * 100, 2), "%"), y = Inf, vjust = 2), size = 2.5) +
+  theme_bw() +
+  xlab("q") +
+  ylab("Eigenvector 1 error") +
+  labs(color = "Method") +
+  scale_color_manual(values = palette) +
+  theme(legend.position = "bottom")
+ggsave(file.path(FIGURES_PATH, "simulation_figure_EV1_error_truncated_q.pdf"), height = 8, width = 7)
+
+
 
 ggplot(min_eigenvalue_df %>% filter(experiment == "q"), aes(x = as.factor(q), y = min_eigenvalue, color = method)) +
   facet_wrap(~paste0("n = ", n, "; ", estimate), scales = "free_y", ncol = 2) +
