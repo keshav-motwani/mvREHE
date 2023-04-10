abd5805f1033bf39382f82f1d5857799a8008933
Keshav Motwani <keshavmot2@gmail.com>
2023-04-10 14:20:45 -0700

    remove everything cholesky based!

--- R/mvREHE.R
+++ R/mvREHE.R
@@ -1,86 +1,33 @@
-#' mvREHE
-#'
-#' @param Y
-#' @param D_list
-#' @param Sigma_list
-#' @param n_lambda
-#' @param lambda_max
-#' @param lambda_min
-#' @param tolerance
-#' @param max_iter
-#' @param L_init_list
-#' @param algorithm
-#'
-#' @return
-#' @export
-#'
-#' @examples
-oracle_mvREHE = function(Y, D_list, Sigma_list, n_lambda = 5, lambda_max = 1, lambda_min = 1e-8, tolerance = 1e-7, max_iter = 10000, L_init_list = NULL, algorithm = "L-BFGS-B") {
-
-  q = ncol(Y)
-
-  lambda_seq = c(log_seq(lambda_max, lambda_min, n_lambda - 1), 0)
-  lambda_grid = do.call(expand.grid, replicate(length(D_list), lambda_seq, simplify = F))
-
-  L_init_list_orig = L_init_list
-
-  loss = numeric(nrow(lambda_grid))
-  loss[] = NA
-
-  for (l in 1:nrow(lambda_grid)) {
-
-    print(l)
-
-    fit = mvREHE(Y, D_list, as.numeric(lambda_grid[l, ]), tolerance, max_iter, L_init_list, algorithm)
-    Sigma_hat = fit$Sigma_hat
-    L_hat = fit$L_hat
-
-    loss[l] = sum(sapply(1:length(Sigma_hat), function(k) norm(Sigma_hat[[k]] - Sigma_list[[k]], "2")))
-
-    if (loss[l] == min(loss, na.rm = T)) best_fit = fit
-
-  }
-
-  lambda = lambda_grid[which.min(loss), ]
-
-  result = best_fit
-  result$oracle_loss = loss
-  result$lambda = lambda
-  result$lambda_grid = lambda_grid
-
-  result
-
-}
-
-#' mvREHE
+#' cv_mvREHE_rank
 #'
 #' @param Y
 #' @param D_list
 #' @param K
-#' @param n_lambda
-#' @param lambda_max
-#' @param lambda_min
+#' @param n_rank
+#' @param rank_max
+#' @param rank_min
 #' @param tolerance
 #' @param max_iter
-#' @param L_init_list
-#' @param algorithm
+#' @param Sigma_init_list
 #'
 #' @return
 #' @export
 #'
 #' @examples
-cv_mvREHE = function(Y, D_list, K, n_lambda = 5, lambda_max = 1, lambda_min = 1e-8, tolerance = 1e-7, max_iter = 10000, L_init_list = NULL, algorithm = "L-BFGS-B") {
+cv_mvREHE_rank = function(Y, D_list, K, n_rank = 5, rank_max = ncol(Y), rank_min = 1, tolerance = 1e-7, max_iter = 10000, Sigma_init_list = NULL) {
 
-  folds = split(sample(1:nrow(Y), nrow(Y)), 1:K)
+  folds = split(1:nrow(Y), rep(1:K, each = ceiling(nrow(Y)/K)))
 
   q = ncol(Y)
 
-  lambda_seq = c(log_seq(lambda_max, lambda_min, n_lambda - 1), 0)
-  lambda_grid = do.call(expand.grid, replicate(length(D_list), lambda_seq, simplify = F))
+  rank_seq = floor(seq(rank_min, rank_max, length.out = n_rank))
+  rank_grid = do.call(expand.grid, replicate(length(D_list), rank_seq, simplify = F))
 
-  loss = numeric(nrow(lambda_grid))
+  Sigma_init_list = Sigma_init_list
 
-  for (l in 1:nrow(lambda_grid)) {
+  loss = numeric(nrow(rank_grid))
+
+  for (l in 1:nrow(rank_grid)) {
 
     print(l)
 
@@ -91,14 +38,11 @@ cv_mvREHE = function(Y, D_list, K, n_lambda = 5, lambda_max = 1, lambda_min = 1e
       print(k)
 
       D_list_mk = lapply(D_list, function(D) D[-folds[[k]], -folds[[k]]])
-      fit = mvREHE(Y[-folds[[k]], ], D_list_mk, as.numeric(lambda_grid[l, ]), tolerance, max_iter, L_init_list, algorithm)
-      Sigma_hat = fit$Sigma_hat
-      L_hat = fit$L_hat
+      Sigma_hat = mvREHE(Y[-folds[[k]], ], D_list_mk, r = as.numeric(rank_grid[l, ]), lambda = NULL, tolerance, max_iter, Sigma_init_list)$Sigma_hat
 
-      Y_tilde_list_k = lapply(1:q, function(j) lapply(1:j, function(m) c(tcrossprod(Y[folds[[k]], j], Y[folds[[k]], m]))))
       X_tilde_k = do.call(cbind, lapply(D_list, function(D) c(D[folds[[k]], folds[[k]]])))
 
-      loss_l = loss_l + length(folds[[k]])^2 * loss2(Y_tilde_list_k, X_tilde_k, Sigma_hat)
+      loss_l = loss_l + length(folds[[k]])^2 * loss3(Y[folds[[k]], ], X_tilde_k, Sigma_hat, rep(0, length(D_list)))
 
     }
 
@@ -106,12 +50,14 @@ cv_mvREHE = function(Y, D_list, K, n_lambda = 5, lambda_max = 1, lambda_min = 1e
 
   }
 
-  lambda = lambda_grid[which.min(loss), ]
+  l = which.min(loss)
+  rank = rank_grid[l, ]
+  print(rank)
 
-  result = mvREHE(Y, D_list, lambda, tolerance, max_iter, L_init_list, algorithm)
+  result = mvREHE(Y, D_list, r = rank, lambda = NULL, tolerance, max_iter, Sigma_init_list)
   result$cv_loss = loss
-  result$lambda = lambda
-  result$lambda_grid = lambda_grid
+  result$rank = rank
+  result$rank_grid = rank_grid
 
   result
 
@@ -128,48 +74,156 @@ log_seq = function(from, to, length) {
 
 }
 
+#' cv_mvREHE_rank
+#'
+#' @param Y
+#' @param D_list
+#' @param K
+#' @param n_rank
+#' @param rank_max
+#' @param rank_min
+#' @param tolerance
+#' @param max_iter
+#' @param Sigma_init_list
+#'
+#' @return
+#' @export
+#'
+#' @examples
+cv_mvREHE_L2 = function(Y, D_list, K, n_lambda = 5, lambda_max = 1e-4, lambda_min = 1e-12, tolerance = 1e-7, max_iter = 10000, Sigma_init_list = NULL) {
+
+  folds = split(1:nrow(Y), rep(1:K, each = ceiling(nrow(Y)/K)))
+
+  q = ncol(Y)
+
+  lambda_seq = c(log_seq(lambda_max, lambda_min, n_lambda), 0)
+  lambda_grid = do.call(expand.grid, replicate(length(D_list), lambda_seq, simplify = F))
+
+  Sigma_init_list = Sigma_init_list
+
+  loss = numeric(nrow(lambda_grid))
+
+  for (l in 1:nrow(lambda_grid)) {
+
+    print(l)
+
+    loss_l = 0
+
+    for (k in 1:K) {
+
+      print(k)
+
+      D_list_mk = lapply(D_list, function(D) D[-folds[[k]], -folds[[k]]])
+      Sigma_hat = mvREHE(Y[-folds[[k]], ], D_list_mk, r = NULL, lambda = as.numeric(lambda_grid[l, ]), tolerance, max_iter, Sigma_init_list)$Sigma_hat
+
+      X_tilde_k = do.call(cbind, lapply(D_list, function(D) c(D[folds[[k]], folds[[k]]])))
+
+      loss_l = loss_l + length(folds[[k]])^2 * loss3(Y[folds[[k]], ], X_tilde_k, Sigma_hat, rep(0, length(D_list)))
+
+    }
+
+    loss[l] = loss_l
+
+  }
+
+  l = which.min(loss)
+  lambda = lambda_grid[l, ]
+  print(lambda)
+
+  result = mvREHE(Y, D_list, r = NULL, lambda = as.numeric(lambda), tolerance, max_iter, Sigma_init_list)
+  result$cv_loss = loss
+  result$lambda = lambda
+  result$lambda_grid = lambda_grid
+
+  result
+
+}
+
 #' mvREHE
 #'
 #' @param Y
 #' @param D_list
 #' @param tolerance
 #' @param max_iter
-#' @param L_init_list
-#' @param algorithm
+#' @param Sigma_init_list
 #'
 #' @return
 #' @export
 #'
 #' @examples
-mvREHE = function(Y, D_list, lambda = NULL, tolerance = 1e-7, max_iter = 10000, L_init_list = NULL, algorithm = "L-BFGS-B") {
+mvREHE = function(Y, D_list, r = NULL, lambda = NULL, tolerance = 1e-7, max_iter = 10000, Sigma_init_list = NULL) {
 
+  n = nrow(Y)
   q = ncol(Y)
+  K = length(D_list)
   objective = numeric(max_iter)
 
-  if (is.null(L_init_list)) {
-    L_list = lapply(1:length(D_list), function(i) t(chol(clusterGeneration::rcorrmatrix(q))))
-  } else if (is.character(L_init_list) && L_init_list == "mvHE") {
-    mvHE_estimate = mvHE(Y, D_list)$Sigma_hat
-    if (all(sapply(mvHE_estimate, function(x) attr(x, "truncated")) == 0)) return(list(Sigma_hat = mvHE_estimate))
-    L_list = lapply(1:length(mvHE_estimate), function(i) t(chol(mvHE_estimate[[i]], pivot = TRUE)))
+  if (is.null(r)) r = rep(q, K)
+  if (is.null(lambda)) lambda = rep(0, K)
+
+  if (is.null(Sigma_init_list)) {
+    Sigma_list = lapply(1:length(D_list), function(i) clusterGeneration::rcorrmatrix(q))
+  } else if (is.character(Sigma_init_list) && Sigma_init_list == "mvHE") {
+    Sigma_list = mvHE(Y, D_list)$Sigma_hat
   } else {
-    L_list = L_init_list
+    Sigma_list = Sigma_init_list
   }
 
-  if (is.null(lambda)) {
-    lambda = rep(0, length(D_list))
-  }
-
-  Y_tilde_list = lapply(1:q, function(j) lapply(1:j, function(m) c(tcrossprod(Y[, j], Y[, m]))))
   X_tilde = do.call(cbind, lapply(D_list, function(D) c(D)))
-  gradient_list = replicate(length(D_list), matrix(0, q, q), simplify = FALSE)
 
-  fit = fit_optim(Y_tilde_list, X_tilde, lambda, max_iter, tolerance, L_list, algorithm)
-  L_list = fit$L_list
-  objective = fit$objective
+  W_list = lapply(1:K, function(x) matrix(0, q, q))
+  compute_W_list(Y, D_list, W_list)
+  Q = compute_Q(D_list)
 
-  return(list(Sigma_hat = lapply(L_list, function(L) tcrossprod(L)),
-              L_hat = L_list,
+  for (iter in 1:max_iter) {
+
+    for (z in 1:K) {
+
+      mat = W_list[[z]]
+      for (k in setdiff(1:K, z)) {
+        mat = mat - Sigma_list[[k]] * Q[k, z]
+      }
+
+      eig = RSpectra::eigs_sym(mat, r[z])
+      Sigma_list[[z]] = eig$vectors %*% (t(eig$vectors) * pmax(eig$values / (Q[z, z] + (lambda[z] * n^2 * q^2)), 0))
+
+    }
+
+    if (iter %% 10 == 0) {
+
+      it = (iter %/% 10)
+
+      objective[it] = loss3(Y, X_tilde, Sigma_list, lambda)
+
+      if (it > 1 && objective[it] > objective[it - 1]) {
+        print(c(objective[1:it]))
+      }
+
+      if (it > 1 && abs(objective[it - 1] - objective[it]) / objective[it - 1] < tolerance) {
+        break
+      }
+
+    }
+
+  }
+
+  return(list(Sigma_hat = Sigma_list,
               objective = objective[objective != 0]))
+
+}
+
+compute_Q = function(D_list) {
+
+  K = length(D_list)
+
+  Q = matrix(NA, K, K)
+
+  for (i in 1:K) {
+    for (j in 1:i) {
+      Q[i, j] = Q[j, i] = sum(D_list[[i]] * D_list[[j]])
+    }
+  }
+
+  return(Q)
 
 }
