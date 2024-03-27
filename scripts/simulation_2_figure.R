0cc04a2a21db69b12c3c6d7968334678312ca4b9
Keshav Motwani <kmotwani.washington@login7.ufhpc>
2024-03-27 14:09:20 -0400

    update figure script

--- scripts/simulation_2_figure.R
+++ scripts/simulation_2_figure.R
@@ -1,11 +1,11 @@
 library(tidyverse)
 
 SIMULATION_ID = 2
-RESULT_PATH = paste0("final_simulation_hcp_results_", SIMULATION_ID)
+RESULT_PATH = paste0("simulation_hcp_results_", SIMULATION_ID)
 FIGURES_PATH = file.path(RESULT_PATH, "figures")
 dir.create(FIGURES_PATH, recursive = TRUE)
 
-methods = c("mvREHE", "mvREHE_cvL2", "mvREHE_cvDR", paste0("DR", c(5), "-mvREML"))
+methods = c("mvHE", "mvREHE", "mvREHE_cvDR", paste0("DR", c(5), "-mvREML"))
 palette = ggsci::pal_aaas("default")(length(methods))
 names(palette) = methods
 options(ggplot2.discrete.colour = palette)
@@ -16,7 +16,6 @@ Sigmas = c("fast", "moderate", "slow")
 
 files = list.files(RESULT_PATH, full.names = TRUE)
 files = files[grepl("rds", files)]
-files = files[grepl("_rInf_", files)]
 
 results = lapply(files, readRDS)
 
@@ -42,8 +41,8 @@ ggsave(file.path(FIGURES_PATH, "simulation_figure_time_n.pdf"), height = 3, widt
 
 ### Spectral error
 
-label = c("hat(Sigma)[E]", "hat(Sigma)[G]")
-names(label) = c("Sigma_0", "Sigma_1")
+label = c("hat(Sigma)[G]", "hat(Sigma)[C]", "hat(Sigma)[E]")
+names(label) = c("Sigma_1", "Sigma_2", "Sigma_0")
 
 Sigmas = apply(expand.grid(label, Sigmas), 1, function(x) paste0(x[2], "~(", x[1], ")"))
 
@@ -56,13 +55,13 @@ ggplot(spectral_error_df %>%
          mutate(facet = paste0(Sigma, "~(", estimate, ")")) %>%
          mutate(facet = factor(facet, levels = Sigmas)),
        aes(x = as.factor(n), y = spectral_error, color = factor(map[gsub(".elapsed", "", method)], levels = names(palette)))) +
-  facet_wrap(~facet, scales = "free_y", nrow = 2, dir = "v", labeller = labeller(facet = label_parsed)) +
+  facet_wrap(~facet, scales = "free_y", nrow = 3, dir = "v", labeller = labeller(facet = label_parsed)) +
   geom_boxplot(outlier.size = 0) +
   theme_bw() +
   xlab("n") +
   labs(color = "Method", linetype = "Method", y = expression("||"*hat(Sigma)[k] - Sigma[k]*"||"[2]), x = "n") +
   theme(legend.position = "bottom")
-ggsave(file.path(FIGURES_PATH, "simulation_figure_spectral_error_n.pdf"), height = 5, width = 8.5)
+ggsave(file.path(FIGURES_PATH, "simulation_figure_spectral_error_n.pdf"), height = 7.5, width = 8.5)
 
 ### Squared error
 
@@ -75,13 +74,13 @@ ggplot(squared_error_df %>%
          mutate(facet = paste0(Sigma, "~(", estimate, ")")) %>%
          mutate(facet = factor(facet, levels = Sigmas)),
        aes(x = as.factor(n), y = squared_error, color = factor(map[gsub(".elapsed", "", method)], levels = names(palette)))) +
-  facet_wrap(~facet, scales = "free_y", nrow = 2, dir = "v", labeller = labeller(facet = label_parsed)) +
+  facet_wrap(~facet, scales = "free_y", nrow = 3, dir = "v", labeller = labeller(facet = label_parsed)) +
   geom_boxplot(outlier.size = 0) +
   theme_bw() +
   xlab("n") +
   labs(color = "Method", linetype = "Method", y = expression("||"*hat(Sigma)[k] - Sigma[k]*"||"["F"]), x = "n") +
   theme(legend.position = "bottom")
-ggsave(file.path(FIGURES_PATH, "simulation_figure_squared_error_n.pdf"), height = 5, width = 8.5)
+ggsave(file.path(FIGURES_PATH, "simulation_figure_squared_error_n.pdf"), height = 7.5, width = 8.5)
 
 ### Squared error on diagonal
 
@@ -94,11 +93,11 @@ ggplot(diag_squared_error_df %>%
          mutate(facet = paste0(Sigma, "~(", estimate, ")")) %>%
          mutate(facet = factor(facet, levels = Sigmas)),
        aes(x = as.factor(n), y = diag_squared_error, color = factor(map[gsub(".elapsed", "", method)], levels = names(palette)), linetype = ifelse(grepl("mv", method), "Multivariate", "Univariate"))) +
-  facet_wrap(~facet, scales = "free_y", nrow = 2, dir = "v", labeller = labeller(facet = label_parsed)) +
+  facet_wrap(~facet, scales = "free_y", nrow = 3, dir = "v", labeller = labeller(facet = label_parsed)) +
   geom_boxplot(outlier.size = 0) +
   theme_bw() +
   xlab("n") +
   labs(color = "Method", linetype = "Method", y = expression("||diag("*hat(Sigma)[k] - Sigma[k]*")||"[2]), x = "n") +
   theme(legend.position = "bottom")
-ggsave(file.path(FIGURES_PATH, "simulation_figure_diag_squared_error_n.pdf"), height = 5, width = 8.5)
+ggsave(file.path(FIGURES_PATH, "simulation_figure_diag_squared_error_n.pdf"), height = 7.5, width = 8.5)
 
